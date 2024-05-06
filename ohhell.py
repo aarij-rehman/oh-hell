825520eebbd7f924aefb6d8f8b84b72055d5d55a
Ram Muthukumaran <rammk1999@gmail.com>
2024-05-05 19:26:59 -0500

    removed data class and replaced with regular class, removed score as field, instead it is a property that is calculated every time it needs to be called

--- ohhell.py
+++ ohhell.py
@@ -1,27 +1,49 @@
 from __future__ import annotations
-from collections import defaultdict
-from dataclasses import dataclass
 
 
-DECK_SIZE = 52
+DECK_SIZE = 10
 SCORE_MULTIPLIER = 10
 with open("VERSION") as file:
     GAME_VERSION = file.read()
 
 
-@dataclass
 class Player:
     name: str
-    score: int = 0
-    bids: int = 0
-    wins: int = 0
+    bids: list[int]
+    wins: list[int]
+
+    def __init__(self, player_name: str):
+        self.name = player_name
+        self.bids = []
+        self.wins = []
+
+    def record_bids(self):
+        player_bid = int(input(f"How many tricks will {self.name} win?: "))
+        self.bids.append(player_bid)
+
+    def record_wins(self):
+        player_win = int(input(f"How many tricks did {self.name} win?: "))
+        self.wins.append(player_win)
+
+    @property
+    def score(self):
+        score = 0
+        for bid, win in zip(self.bids, self.wins):
+            score += self._compute_score(bid, win)
+        return score
+
+    def _compute_score(self, bid: int, win: int) -> int:
+        if bid != win:
+            return abs(win - bid) * -10
+        return 20 + (10 * bid)
+
 
 
 def enter_players(num_players: int):
     players = []
     for i in range(num_players):
         players.append(
-            Player(str(input("Enter the player's name: ")))
+            Player(str(input(f"Enter a name for player #{i+1}: ")).lower())
         )
     print("=====================================")
     return players
@@ -34,44 +56,47 @@ def enter_player_guesses(players: list[Player], round: int):
         player = players[(i + round) % len(players)]
         if i == len(players) - 1 and total_bids <= allowed_bids:
             print(f"The next player CANNOT GUESS {allowed_bids - total_bids}")
-        player.bids = int(input(f"How many tricks will {player.name} win?: "))
-        total_bids += player.bids
-
+        player.record_bids()
+        total_bids += player.bids[-1]
 
 def enter_tricks_won(players: list[Player], round: int):
     for i in range(len(players)):
         player = players[(i+round) % len(players)]
-        player.wins = int(input(f"How many tricks did {player.name} win?: "))
+        player.record_wins()
 
 
-def update_scores(players: list[Player]):
-    for player in players:
-        if player.bids != player.wins:
-            player.score -= abs(player.wins - player.bids) * 10
-        else:
-            player.score += 20 + (10 * player.bids)
-
+def update_scores(players: list[Player]) -> list:
     print("Current Scores")
-    for player in sorted(players, key=lambda x:x.score, reverse=True):
-        print(f"{player.name}: {player.score} points")
+    player_scores = []
+    for player in players:
+        player_scores.append((player.name, player.score))
+
+    # print the scores in order after each round
+    for pair in sorted(player_scores, key=lambda x:x[1], reverse=True):
+        print(f"{pair[0]}: {pair[1]} points")
+    
+    return player_scores
 
 
-def display_winner(players: list[Player]):
-    players.sort(key=lambda x:x.score, reverse=True)
-    print(f"The winner is {players[0].name}! They scored {players[0].score} points!")
+def display_winner(current_scores: list[tuple[Player, int]]):
+    current_scores.sort(key=lambda x:x[1], reverse=True)
+    print(f"The winner is {current_scores[0][0]}! They scored {current_scores[0][1]} points!")
+    print(f"The runner up is {current_scores[1][0]}! They scored {current_scores[1][1]} points!")
+    print(f"And in third place is {current_scores[2][0]}! They scored {current_scores[2][1]} points!")
 
 
 def game_loop(players: list[Player], max_rounds: int):
+    current_scores = []
     for round in range(max_rounds):
         print(f"Round {round+1}")
         enter_player_guesses(players, round)
         print("------------------")
         enter_tricks_won(players, round)
         print("------------------")
-        update_scores(players)
+        current_scores = update_scores(players)
         print("=====================================")
 
-    display_winner(players)
+    display_winner(current_scores)
 
 
 def main():
